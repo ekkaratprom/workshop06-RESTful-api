4c1aeba615b5eaba2ffd450f343e81f09516dda1
Ekkarat Promrit <ekkaratpro@mathregit.group>
2020-08-05 13:15:36 +0700

    :heavy_plus_sign: STEP: 02

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,17 +8,26 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @GetMapping("/users")
-    public List<UserResponse> getAllUser(){
-        List<UserResponse> userResponseList = new ArrayList<>();
-        userResponseList.add(new UserResponse(1, "User 1"));
-        userResponseList.add(new UserResponse(2, "User 2"));
-        userResponseList.add(new UserResponse(3, "User 3"));
-        return userResponseList;
+//    @GetMapping("/users")
+//    public List<UserResponse> getAllUser(){
+//        List<UserResponse> userResponseList = new ArrayList<>();
+//        userResponseList.add(new UserResponse(1, "User 1"));
+//        userResponseList.add(new UserResponse(2, "User 2"));
+//        userResponseList.add(new UserResponse(3, "User 3"));
+//        return userResponseList;
+//    }
+
+
+    @GetMapping("/user?")
+    @ResponseBody
+    public String getUserWithPages(@RequestParam(required = false,defaultValue = "2") String page,
+                                   @RequestParam(name = "item_per_page",required = false,defaultValue = "15") String itemPerPage){
+        return "page=" + page + "item_per_page=" + itemPerPage;
     }
 
     @GetMapping("/users/{id}")
     public UserResponse getUserByID(@PathVariable int id){
         return new UserResponse(id,"User " + id);
     }
+
 }
